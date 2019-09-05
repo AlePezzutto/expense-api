fe48ff7d24477e43e95c3fc1aa59de3c551a7935
Alexandre Pezzutto <ale.pezzutto@gmail.com>
2019-09-05 15:42:07 -0300

    Adicionando refresh token

--- expense-api/src/main/java/br/com/zoot/api/config/AuthorizationServerConfig.java
+++ expense-api/src/main/java/br/com/zoot/api/config/AuthorizationServerConfig.java
@@ -8,9 +8,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.provider.token.AccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -28,10 +26,11 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
 			.withClient("Angular")
-			.secret("{noop}Buceta")
+			.secret("{noop}Angul@R")
 			.scopes("read", "write")
-			.authorizedGrantTypes("password")
-			.accessTokenValiditySeconds(1800); // 30 minutos
+			.authorizedGrantTypes("password", "refresh_token")
+			.accessTokenValiditySeconds(60)
+			.refreshTokenValiditySeconds(3600*24); // 1 dia
 	}
 	
 	@Override
@@ -39,6 +38,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		endpoints
 			.tokenStore(tokenStore())
 			.accessTokenConverter(accessTokenConverter())
+			.reuseRefreshTokens(false)
 			.authenticationManager(authenticationManager);
 	}
 	
